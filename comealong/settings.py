8416587a2faec48812d8d7e9465bcb82bc2aeef7
Guilherme Graca <ggracase@gmail.com>
2014-11-13 05:01:23 +0000

    Added webserver config

--- comealong/settings.py
+++ comealong/settings.py
@@ -99,14 +99,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "ui/static"),
 )
 
 #Media
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 MEDIAFILES_DIRS = (
     os.path.join(BASE_DIR, "media"),
 )
