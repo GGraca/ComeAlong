0b91fd900ed19c4811f9bea5ad28ebf7685e4f73
Guilherme Graca <ggracase@gmail.com>
2014-12-21 22:43:21 +0000

    Added login, register and is_user_available views. Organized some code

--- comealong/views.py
+++ comealong/views.py
@@ -1,5 +1,7 @@
 from project.models import Project
 from django.views.generic import TemplateView
+from my_user.models import MyUser
+from django.contrib.auth import authenticate
 
 class Index(TemplateView):
     template_name = "index.html"
@@ -12,3 +14,19 @@ class Index(TemplateView):
             #Project.objects.get(id=3),
         ]
         return context
+
+def is_username_available(request):
+    if MyUser.objects.filter(username=request.username).count() > 0:
+        return HttpResponse("False")
+    return HttpResponse("True")
+
+
+def login(request):
+    user = authenticate(username=request.username, password=request.password)
+    if user is None:
+        return HttpResponse("error")
+    return HttpResponse("success")
+
+def register(request):
+    user = User.objects.create_user(request.username, request.email, request.password, first_name=request.first_name, last_name=request.last_name)
+    login(request)
--- my_user/models.py
+++ my_user/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class Profile(models.Model):
+	#Relations
+
+
+	#Atributes
 	avatar = models.ImageField(upload_to = 'img/',  default = 'default/img/user.png')
 	description = models.TextField()
 
@@ -13,6 +18,8 @@ class Profile(models.Model):
 	github = models.URLField(max_length=100, null=True, default=None)
 	website = models.URLField(max_length=100, null=True, default=None)
 
+
+	#Functions
 	class Meta:
 		abstract = True
 
@@ -24,25 +31,28 @@ class MyUser(AbstractUser, Profile, models.Model):
 	"""applications"""
 	"""projects_following"""
 
+
 	#Atributes
 	id = models.AutoField(primary_key=True)
 	"""name""" #from AbstractUser
 	"""username""" #from AbstractUser
 
+
+	#Functions
 	def __unicode__(self):
 		return self.username
 
 	def get_absolute_url(self):
 		return u'/users/%s' % self.username
 
+
 class Group(Profile, models.Model):
+	#Relations
+
+
+	#Atributes
 	id = models.AutoField(primary_key=True)
 
 
-#2F3954
-#C1E8E3
-#EFEFEF
-#C1C1B5
-#9E9E9C
-#recruitment
-#oportunities
+	#Functions
+	
