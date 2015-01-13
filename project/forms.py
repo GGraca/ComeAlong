403f33c28fd7302375744c7fa4a1be6565b91e2d
Guilherme Graca <ggracase@gmail.com>
2015-01-13 15:27:57 +0000

    Form for cover_image and feature list

--- project/forms.py
+++ project/forms.py
@@ -7,7 +7,7 @@ class ProjectForm(forms.ModelForm):
     description = forms.CharField(widget=RedactorEditor())
     class Meta:
         model = Project
-        fields = ("title",  "display_image", "short_description", "description")
+        fields = ("title",  "display_image", "cover_image", "short_description", "description")
 
 class ApplicationForm(forms.ModelForm):
 
